a7c935a33ee06201128e0929482c50aab096d3a2
XaveScor <xavescor@gmail.com>
2021-02-10 19:36:35 +0300

    fix ts typings

--- src/debounce.ts
+++ src/debounce.ts
@@ -5,12 +5,12 @@ const defaultOptions: Options = {
   maxTimeout: 1000,
 };
 
-export function debounce<
-  Args extends any[],
-  Func extends (...args: Args) => void
->(func: Func, options = defaultOptions) {
+export function debounce<Func extends (...args: any[]) => void>(
+  func: Func,
+  options = defaultOptions
+) {
   let handler: any;
-  return (...args: Args): void => {
+  return (...args: Parameters<Func>): void => {
     clearTimeout(handler);
     handler = setTimeout(() => {
       func(...args);
